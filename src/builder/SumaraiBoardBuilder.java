f1b98061f0b566bc22896c72a961183a575cae78
Mick Jari <jari.zilverentant@gmail.com>
2023-05-11 12:41:41 +0200

    jigsaw and samurai setup

---
+++ src/builder/SumaraiBoardBuilder.java
@@ -0,0 +1,15 @@
+package builder;
+
+import java.util.List;
+
+import board.Board;
+
+public class SumaraiBoardBuilder implements IBoardBuilder {
+
+    @Override
+    public Board build(List<Integer> nums) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'build'");
+    }
+
+}
